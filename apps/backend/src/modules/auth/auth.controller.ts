cb79fd5043c7605c00245af2e11eb9ed4f6a547e
Sarwagya Singh <sarwagyasingh69@gmail.com>
2025-11-26 01:47:05 +0100

    fix(backend): fix public decorator import path

--- apps/backend/src/modules/auth/auth.controller.ts
+++ apps/backend/src/modules/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Post } from "@nestjs/common";
+import { Public } from "../../common/decorators/public.decorator";
 import { AuthService } from "./auth.service";
 import { LoginDto } from "./dto/login.dto";
 import { RegisterDto } from "./dto/register.dto";
@@ -7,6 +8,7 @@ import { RegisterDto } from "./dto/register.dto";
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @Post("register")
   async register(@Body() registerDto: RegisterDto) {
     return this.authService.register(
@@ -16,6 +18,7 @@ export class AuthController {
     );
   }
 
+  @Public()
   @Post("login")
   async login(@Body() loginDto: LoginDto) {
     const user = await this.authService.validateUser(
